0fec80a9466984d94d95e0316f58015c67c62f80
ExNoise <36914442+ExNoise@users.noreply.github.com>
2021-09-05 13:16:43 +0200

    Add files via upload
    reached a point where I'm hungry lel

--- Awc Brutto/js/main.js
+++ Awc Brutto/js/main.js
@@ -3,7 +3,7 @@ var span=document.getElementsByTagName('span');
 var div=document.querySelectorAll('div.carousel');
 var l=0;
 
-console.log(span)
+console.log(span.length)
 
 
 span[1].onclick = function(){
@@ -11,12 +11,12 @@ l++;
 console.log("Clicked");
 for(var i of div) {
        /* todo si puo migliorare con un calcolo  */
-        if (l==0){i.style.left = "0px";}
-       if (l==1){i.style.left = "-740px";}
-        if (l==2){i.style.left = "-1480px";}
-       if (l==3){i.style.left = "-2220px";}
-       if (l==4){i.style.left = "-2960px";}
-       if (l>4) {l=4;};
+    if (l==0){i.style.left = "0px";}
+    if (l==1){i.style.left = "-740px";}
+    if (l==2){i.style.left = "-1480px";}
+    if (l==3){i.style.left = "-2220px";}
+    if (l==4){i.style.left = "-2960px";}
+    if (l>4) {l=4;};
    }
 }
 
@@ -26,12 +26,12 @@ span[0].onclick = ()=>{
        l--;
        for(var i of div) {
        /* todo si puo migliorare con un calcolo  */
-        if (l==0){i.style.left = "0px";}
-        if (l==1){i.style.left = "-740px";}
-       if (l==2){i.style.left = "-1480px";}
-       if (l==3){i.style.left = "-2220px";}
-       if (l==4){i.style.left = "-2960px";}
-      if (l<0) {l=0;}
+    if (l==0){i.style.left = "0px";}
+    if (l==1){i.style.left = "-740px";}
+    if (l==2){i.style.left = "-1480px";}
+    if (l==3){i.style.left = "-2220px";}
+    if (l==4){i.style.left = "-2960px";}
+    if (l<0) {l=0;}
        }
    
 }   
