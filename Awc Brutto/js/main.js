aa867e3f0f30f65a9049e5c63994d7bbe1cb0820
ExNoise <36914442+ExNoise@users.noreply.github.com>
2021-10-04 13:31:14 +0200

    Add files via upload
    updated 04-10-2021

--- Awc Brutto/js/main.js
+++ Awc Brutto/js/main.js
@@ -7,6 +7,67 @@ function Load(mode){
    if (mode==0){
 
 
+      //dictionary to know how many part we need
+      let dict={0:"MyFilms",1:"Wishlist",2:"Popular"}
+
+      document.getElementById("content-container").innerHTML ="";
+      //create in an automatic way the 3 parts
+      
+
+      //index get from APIs
+      var index=""
+      
+      for(i = 0;i<Object.keys(dict).length;i++)
+      {
+
+         document.getElementById("content-container").innerHTML += `
+               <h1>New Arrival</h1>
+               <section id="section `+dict[i]+`"></section>`;
+
+               for(j=0;j<13;j++){
+
+                  index="";
+
+                  //index get from APIs
+                  index="";
+
+                  //switch dict[i] to id of the movie
+                  document.getElementById("section "+dict[i]).innerHTML += 
+                  //`<div class=container_`+dict[i]+`>
+
+                  //ok, sooo it works in this way, i need to use a custom dataset called data-<<something>> 
+                  //and is called in js via .dataset.<<something>> 
+                  `<div class="bg-img carousel_card" id="carousel_card_`+dict[i]+` " data-index="`+index+`" >
+                  `+j+`
+                  </div> `;
+
+
+               }
+
+
+      }
+
+      document.addEventListener('click',function(e){
+
+         console.log(e.target);
+         if(e.target && e.target.id.includes("carousel_card")){
+            //do something
+            openPopup(e.target.dataset.index);
+
+
+
+
+
+
+            //popup for the search
+         }else if(e.target.id.includes("content-container")&& localStorage.getItem("mode")==0){
+
+            openPopup(e.target.dataset.index);
+
+           
+         }
+
+      });
 
 
 
@@ -16,6 +77,7 @@ function Load(mode){
 
 
    }
+
 
 }
 
@@ -68,7 +130,7 @@ trying.onclick=()=>{
       console.log(e.target);
       if(e.target && e.target.id.includes("carousel_card")){
          //do something
-         openPopup();
+         openPopup(e.target.dataset.index);
          var index_film=e.target.dataset.index;
 
 
@@ -135,12 +197,20 @@ window.localStorage.removeItem("try",JSON.stringify(array));
 //var col=document.getElementById(‘Mydiv’);
 //col.innerHTML=’<div onclick=“alert(2);test(123);”>Text</div>’;
 
-function openPopup(){
+function openPopup(filmId){
    //toogle blur and popup
-   document.getElementsByClassName("blur")[0].style.webkitFilter="blur(69px)";
+   document.getElementsByClassName("blur")[0].style.webkitFilter="blur(60px)";
    document.getElementsByClassName("vanish")[0].style.display="block";
+   
+   var posterPath=getPoster(filmId).posters.file_path;
 
-   //
+
+   document.getElementById("poster_popup").src=posterPath;
+
+
+
+   //ok here just put the things like the generation of the poster and the edit of the text
+   //put the status of the wishlist button toogle
 
    
 }
@@ -151,6 +221,23 @@ function closePopup(){
    document.getElementsByClassName("vanish")[0].style.display="none";
 
    //
-   
+
 
 }
+
+function search(){
+   var result=search(document.getElementbyId("search_bar").value);
+
+   document.getElementById("content-container").innerHTML ="";
+
+   var len=result.results.length;
+
+   for(i=0;i<len;i++){
+      document.getElementById("content-container").innerHTML +=`<div><img src="`+result.results[i].poster_path+`" alt="`+ result.results[i].title+`"></img></div>`;
+   
+   } 
+
+
+
+}
+
